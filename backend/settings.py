d0729b17d62ef0683ffbda075b52d38ed9866306
Huy Chau <huy.chau@besmartee.com>
2025-08-09 22:26:26 +0700

    Allow host

--- backend/settings.py
+++ backend/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = 'django-insecure-mk2xtk=yk6e7g%ro2xoh-h+7e0=-@s(*ri+j@6qf61cu$=l4e%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['139.180.209.110', '127.0.0.1', 'localhost','172.30.1.16','127.0.0.1','124.216.166.162','3.26.21.10']
+ALLOWED_HOSTS = ['139.180.209.110', '127.0.0.1', 'localhost','172.30.1.16','127.0.0.1','124.216.166.162','3.26.21.10','photobooth-api-c2ku.onrender.com']
 
 
 # Application definition
