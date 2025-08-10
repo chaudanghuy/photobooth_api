90c2b2ddf753e8e5c8615872ed943a44ca31d7e3
Huy Chau <huy.chau@besmartee.com>
2025-08-10 15:28:06 +0700

    Fix static issues

--- backend/settings.py
+++ backend/settings.py
@@ -192,15 +192,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
-else:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # nơi collectstatic sẽ copy file vào
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # nơi bạn lưu CSS/JS gốc
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
